a890f7b75b778e1db5ca59ec1a28a5469c813873
Jyotirmoy-Roy <jyotirmoyroy340@gmail.com>
2023-02-26 19:59:59 +0600

    First Commit

---
+++ Beginner/return_the_sum_of_two_numbers.js
@@ -0,0 +1,28 @@
+/*
+Q1. Return the sum of Two Numbers
+
+-Create a function that takes two argumnets
+as function and add return their sum
+
+
+****
+addition(3, 2) -> 5
+addition(-3, -6) -> -9
+addition(7, 3) -> 10
+****
+
+*/
+
+// Solution
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+let result;
+
+result = add(2, 3);
+result = add(-3, -6)
+result = add(7, 3)
+
+console.log(result);
